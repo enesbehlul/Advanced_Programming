503468cdc57ea5fae6f50e5bbe3ae0187f2fb57c
Enes Behlül <ebehlul.yenidunya@stu.fsm.edu.tr>
2018-11-20 14:33:05 +0300

    some improvements

--- HW3/anmtn.js
+++ HW3/anmtn.js
@@ -1,6 +1,8 @@
 const 
   t1 = { transform: [
- "translate(100px,0px)", "translate(-100px,0px)","translate(100px,0px)"]}
+ "translate(500px,0px)", "translate(-200px,0px)","translate(500px,0px)"]}
+ t2 = {transform: ["rotate(0deg)", "rotate(180deg)", "rotate(360deg)"]}
+ t3 = {transform: ["translateY(0px)", "translateY(-50px)", "translateY(50px)", "translateY(0px)"]}
 function pikachu(){
 	var pikachu = document.getElementById("pikachu");
 	var str = document.getElementById("speed").value;
@@ -12,4 +14,25 @@ function pikachu(){
             });	
 
 }
-pikachu();
+function rotate(){
+	var pikachu = document.getElementById("pikachu");
+	var str = document.getElementById("speed").value;
+	console.log(duration);
+	var duration = parseInt(str);
+	pikachu.animate(t2,{ 
+            duration,
+            iterations: Infinity
+            });	
+
+}
+function picaDown() {
+var pikachu = document.getElementById("pikachu");
+	var str = document.getElementById("speed").value;
+	console.log(duration);
+	var duration = parseInt(str);
+	pikachu.animate(t3,{ 
+            duration,
+            iterations: Infinity
+            });	
+
+}
