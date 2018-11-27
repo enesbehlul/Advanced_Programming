00fb2a996fa8b15c5437c6a17ee67f01a450bb44
Enes Behlül <ebehlul.yenidunya@stu.fsm.edu.tr>
2018-11-27 16:45:22 +0300

    pwa update

--- sw.js
+++ sw.js
@@ -1,16 +1,21 @@
-const CACHE ='JS'
+
+const CACHE ='icon'
+function installCB(e) {
+  console.log('install oldu', e.request);
+}
+self.addEventListener('install', installCB)
 
 function save(req, resp) {
   return caches.open(CACHE)
   .then(cache => {
     cache.put(req, resp.clone());
     return resp;
-  }) 
+  })
   .catch(console.log)
 }
 function fetchCB(e) { //fetch first
   let req = e.request
-  console.log('BLM', req.url);
+  console.log('JS', req.url);
   e.respondWith(
     fetch(req).then(r2 => save(req, r2))
     .catch(() => { return caches.match(req).then(r1 => r1) })
